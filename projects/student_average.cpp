1e0746b1670e02f2b70cdc48eaf4a26baf957c42
Tola-byte <adetolaisaiah17@gmail.com>
2025-11-20 17:25:10 +0100

    feat: simple projects calculator

---
+++ projects/student_average.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+//#include <vector>
+
+
+class Student{
+    public:
+     std::vector<int> scores;
+
+        void addScores(int score){
+           
+            scores.push_back(score);
+        }
+
+        double returnAverage(){
+            int sum = 0;
+            for(int i = 0; i < scores.size(); i++){
+                 sum = sum + scores[i];
+            }
+            return sum / scores.size();    
+
+        }
+
+};
+
+int main(){
+    Student student;
+    int amount;
+    int score;
+
+    std::cout << "How many subjects do you want to submit?" << std::endl;
+    std::cin >> amount;
+
+    for(int i=0; i < amount; ++i){
+        std::cout << "What's course score for course "<< i << std::endl;
+        std::cin >> score;
+        student.addScores(score);
+    }
+    
+    std::cout << "average is " << student.returnAverage() << std::endl;
+    return 0;
+}
